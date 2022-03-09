546978922154651636a906f260f66847e44a3d60
Emin Cingöz <emincingoz37@gmail.com>
2022-03-09 16:19:31 +0300

    Top navigation bar and menu controller added

--- lib/widgets/layout.dart
+++ lib/widgets/layout.dart
@@ -3,16 +3,18 @@ import 'package:web_dashboard/helpers/responsiveness.dart';
 import 'package:web_dashboard/widgets/large_screen.dart';
 import 'package:web_dashboard/widgets/small_screen.dart';
 
+import 'top_navigation_bar.dart';
+
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
+  SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
+      key: scaffoldKey,
+      appBar: topNavigationBar(context, scaffoldKey),
+      drawer: Drawer(),
       body: const ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
